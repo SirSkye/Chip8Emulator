bc84fb28f7aacf8e147a2dddc99b0472714c15b0
SirSkye <aisha278151@gmail.com>
2026-05-01 16:12:20 -0400

    Added new  program

--- src/main.c
+++ src/main.c
@@ -53,7 +53,7 @@ void SDLCALL audio_callback(void *userdata, SDL_AudioStream *stream, int additio
 
 int main(int argc, char *argv[]) {
     struct chip8 *chip = chip8_create();
-    if (chip8_load(chip, ".\\programs\\7-beep.ch8") != 0) {
+    if (chip8_load(chip, ".\\programs\\tetris.ch8") != 0) {
         printf("CLOSING");
         free(chip);
         exit(1);
